977daf6ae2a83ea8593d863c9135906eaa4e94ec
ilyana19 <aries_2152@hotmail.com>
2018-08-09 15:32:32 -0400

    now shows all entries on the index page

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,6 +1,7 @@
 class EntriesController < ApplicationController
   def index
     # here we'll define some @instance_variables to store data from the database for the views to use
+    @entries = Entry.all
     render :index
   end
 
